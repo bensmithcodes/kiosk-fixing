9d21a32ca3492b9a6297dafa9535624d4003178e
Ben Smith <bencodes28@gmail.com>
2023-08-01 20:23:32 -0500

    changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Box, Card, CardActionArea, Typography } from '@material-ui/core';
 import TouchAppIcon from '@material-ui/icons/TouchApp';
-import { useStyles } from '../styles';
-import Logo from '../components/Logo';
+import { makeStyles } from '@material-ui/core/styles'; // Import makeStyles
+
+// Define useStyles outside of the HomeScreen component
+const useStyles = makeStyles((theme) => ({
+  // Other styles...
+
+  card: {
+    maxWidth: '800px', // Adjust this value to make the card wider
+    margin: '0 auto', // Center the card horizontally
+  },
+}));
+
 export default function HomeScreen(props) {
   const styles = useStyles();
   return (
-    <Card>
+    <Card className={styles.card}>
       <CardActionArea onClick={() => props.history.push('/choose')}>
         <Box className={[styles.root, styles.red]}>
           <Box className={[styles.main, styles.center]}>
@@ -22,7 +32,6 @@ export default function HomeScreen(props) {
             <TouchAppIcon fontSize="large"></TouchAppIcon>
           </Box>
           <Box className={[styles.center, styles.green]}>
-         
             <Typography variant="h5" component="h3" className={styles.footer} >
               Touch to Start!
             </Typography>
